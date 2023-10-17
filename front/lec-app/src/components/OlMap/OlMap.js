43aa8c86df5229aeba465088b1296f9ec8995471
Omar Alvarez <omar.alvarez@fundacionctic.org>
2023-10-17 11:10:25 +0200

    feat: To change color when hover on a municipality

--- front/lec-app/src/components/OlMap/OlMap.js
+++ front/lec-app/src/components/OlMap/OlMap.js
@@ -5,6 +5,15 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
 
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import GeoJSON from "ol/format/GeoJSON";
+import { Style, Stroke } from "ol/style";
+
+import Select from "ol/interaction/Select";
+
+import { Fill } from "ol/style";
+
 const OlMap = (props) => {
   const mapRef = useRef();
   const [map, setMap] = React.useState(null);
@@ -21,30 +30,91 @@ const OlMap = (props) => {
         center: [0, 0],
         zoom: 2,
       }),
-      controls: []
+      controls: [],
     });
 
+    // Creación de la capa de municipios
+    const municipalityLayer = new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: "/Municipios_IGN.geojson",
+      }),
+      style: new Style({
+        stroke: new Stroke({
+          color: "red",
+          width: 2,
+        }),
+        fill: new Fill({ 
+          color: 'rgba(255,255,255,0.1)'  // Relleno transparente
+        }),
+      }),
+    });
+
+    initialMap.addLayer(municipalityLayer);
+
+    // Estilo de selección
+    const selectStyle = new Style({
+      stroke: new Stroke({
+        color: "red",
+        width: 2,
+      }),
+      fill: new Fill({
+        color: "rgba(255, 0, 0, 0.1)", 
+      }),
+    });
+
+    const select = new Select({
+      condition: (event) => {
+        let selected = false;
+        initialMap.forEachFeatureAtPixel(event.pixel, (feature, layer) => {
+          if (layer === municipalityLayer) {
+            selected = true;
+          }
+        });
+        return selected;
+      },
+      layers: [municipalityLayer],
+      style: selectStyle,
+    });
+    
+
+    initialMap.addInteraction(select);
+
     setMap(initialMap);
+
+    // Limpieza: remueve la interacción y la capa al desmontar el componente
+    return () => {
+      initialMap.removeInteraction(select);
+      initialMap.removeLayer(municipalityLayer);
+    };
   }, []);
 
-  const panToLocation = useCallback(async (locName) => {
-    try {
-      const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&limit=1&q=${locName}`);
-      const json = await response.json();
-      if (json && json.length > 0) {
-        const location = json[0];
-        const coordinates = fromLonLat([parseFloat(location.lon), parseFloat(location.lat)]);
-        map.getView().animate({
-          center: coordinates,
-          zoom: 11
-        });
-      } else {
-        alert('Ubicación no encontrada');
+  const panToLocation = useCallback(
+    async (locName) => {
+      try {
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/search?format=json&limit=1&q=${locName}`
+        );
+        const json = await response.json();
+        if (json && json.length > 0) {
+          const location = json[0];
+          const coordinates = fromLonLat([
+            parseFloat(location.lon),
+            parseFloat(location.lat),
+          ]);
+          map.getView().animate({
+            center: coordinates,
+            zoom: 11,
+          });
+        } else {
+          alert("Ubicación no encontrada");
+        }
+      } catch (error) {
+        console.error("Error en la búsqueda:", error);
       }
-    } catch (error) {
-      console.error('Error en la búsqueda:', error);
-    }
-  }, [map]);
+    },
+    [map]
+  );
 
   useEffect(() => {
     if (props.location && map) {
