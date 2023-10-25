9548e76efe7090d510606638f679b53dcda1f75d
Omar Alvarez <omar.alvarez@fundacionctic.org>
2023-10-25 16:37:45 +0200

    data: To convert centroid coordinates to 'EPSG:4326'

--- front/lec-app/src/App.js
+++ front/lec-app/src/App.js
@@ -6,10 +6,45 @@ import DTable from "./components/UI/DataTable/DTable";
 function App() {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [tableData, setTableData] = useState([]);
-  const [availables, setAvailables] = useState([]); // Declaración de availables
+  const [availables, setAvailables] = useState([]);
 
   const handleRowClick = (row) => {
     console.log("Click en fila", row);
+    
+    // Asumamos que las coordenadas que deseas convertir están en las propiedades `row.lat` y `row.lng`
+    const coordinatesToConvert = {
+      lat: row.lat,
+      lng: row.lng,
+    };
+
+    // URL de tu servicio del backend que convierte coordenadas
+    const apiUrl = `${process.env.REACT_APP_COORDINATES_API_URL}/25830/to/4326`;
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(coordinatesToConvert), // Convertimos las coordenadas a un string JSON para enviarlas en el cuerpo de la solicitud
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json(); // Devuelve las nuevas coordenadas como JSON
+      })
+      .then((data) => {
+        // Aquí puedes manejar las coordenadas convertidas. Por ejemplo:
+        //const newLat = data.newLat;
+        //const newLng = data.newLng;
+        console.log(data);
+
+        // Aquí puedes hacer lo que necesites con las coordenadas convertidas, por ejemplo:
+        // setSelectedLocation({ lat: newLat, lng: newLng });
+      })
+      .catch((error) => {
+        console.error("Hubo un error al convertir las coordenadas:", error);
+      });
   };
 
   const handleLocationSelected = (location) => {
@@ -33,7 +68,7 @@ function App() {
 
   useEffect(() => {
     // Realiza la llamada GET a la API del backend para obtener los datos de availables
-    const url = `${process.env.REACT_APP_MUNICIPALITIES_WITH_DATA}?withData=true`;
+    const url = `${process.env.REACT_APP_MUNICIPALITIES_API_URL}?withData=true`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
