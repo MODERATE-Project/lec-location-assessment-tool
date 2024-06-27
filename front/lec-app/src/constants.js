9506b2e12b4d04985931494d609ea709b7b58037
Sergio Flórez <sergio.florez@fundacionctic.org>
2024-06-27 09:00:56 +0200

    fix: restored default services URI for development

--- front/lec-app/src/constants.js
+++ front/lec-app/src/constants.js
@@ -2,9 +2,9 @@
 // const REACT_APP_BUILDINGS_API_URL = 'http://localhost:5001/buildings'
 // const REACT_APP_GEOSERVER_API_URL = 'http://localhost:8080/geoserver'
 
-const VITE_MUNICIPALITIES_API_URL = import.meta.env.VITE_MUNICIPALITIES_API_URL // || 'http://localhost:5000/municipalities';
-const VITE_BUILDINGS_API_URL = import.meta.env.VITE_BUILDINGS_API_URL // || 'http://localhost:5001/buildings';
-const VITE_GEOSERVER_API_URL = import.meta.env.VITE_GEOSERVER_API_URL // || 'http://localhost:8080/geoserver';
+const VITE_MUNICIPALITIES_API_URL = import.meta.env.VITE_MUNICIPALITIES_API_URL || 'http://localhost:5000/municipalities';
+const VITE_BUILDINGS_API_URL = import.meta.env.VITE_BUILDINGS_API_URL || 'http://localhost:5001/buildings';
+const VITE_GEOSERVER_API_URL = import.meta.env.VITE_GEOSERVER_API_URL || 'http://localhost:8080/geoserver';
 
 
 export { VITE_BUILDINGS_API_URL, VITE_MUNICIPALITIES_API_URL, VITE_GEOSERVER_API_URL }
