dd03e2b0d69a62d67d38a5862370ca4d3bfa09ac
Sergio Flórez <sergio.florez@fundacionctic.org>
2024-07-18 15:06:01 +0200

    feat: makes the SortingCriteriaSelector component  collapsable

--- front/lec-app/src/components/UI/SortingCriteriaSelector/SortingCriteriaSelector.jsx
+++ front/lec-app/src/components/UI/SortingCriteriaSelector/SortingCriteriaSelector.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import './SortingCriteriaSelector.css';
 import Loader from '../Loader';
+import { Collapse } from 'react-collapse';
+
 
 const SortingCriteriaComponent = ({ onSort, isLoading }) => {
 
+  const [isOpened, setIsOpened] = useState(false);
   const [variableImportance, setVariableImportance] = useState({
     Rent: 0,
     Age: 0,
@@ -27,34 +30,45 @@ const SortingCriteriaComponent = ({ onSort, isLoading }) => {
       ...variableImportance,
       [variable]: importanceValue,
     });
+
   };
   const handleSort = () => {
     onSort(variableImportance);
   };
 
+  const toggleAccordion = () => {
+    setIsOpened(!isOpened);
+  };
+
+
   return (
     <div className="sorting-criteria-container">
-      <h2>Sorting Criteria</h2>
-      <p>Select the importance of each variable on a scale from 0 to {upperLimit}</p>
-      <div className="variable-importance">
-        {Object.entries(variableImportance).map(([variable, value]) => (
-          <div key={variable} className="variable-row">
-            <label htmlFor={variable}>{variable}</label>
-            <input
-              type="number"
-              id={variable}
-              min="0"
-              max="10"
-              value={value}
-              onChange={(e) => handleImportanceChange(variable, e.target.value)}
-            />
+      <div className="sorting-criteria-header" onClick={toggleAccordion}>
+        <h2>Sorting Criteria</h2>
+        <p>Select the importance of each variable on a scale from 0 to {upperLimit}</p>
+
+        <Collapse isOpened={isOpened}>
+          <div className="sorting-criteria-content" onClick={(e) => e.stopPropagation()}>
+            {Object.entries(variableImportance).map(([variable, value]) => (
+              <div key={variable} className="variable-row">
+                <label htmlFor={variable}>{variable}</label>
+                <input
+                  type="number"
+                  id={variable}
+                  min="0"
+                  max="10"
+                  value={value}
+                  onChange={(e) => handleImportanceChange(variable, e.target.value)}
+                />
+              </div>
+            ))}
+            <button className="sort-button dark" onClick={handleSort}>
+              {isLoading && <Loader />}
+              Sort Table
+            </button>
           </div>
-        ))}
+        </Collapse>
       </div>
-      <button className="sort-button dark" onClick={handleSort}>
-        { isLoading && <Loader /> }
-        Sort Table
-        </button>
     </div>
   );
 };
