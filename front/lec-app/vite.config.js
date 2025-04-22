365a6ea35a43872a7e06e8cbcc098c02e233e697
Sergio Flórez <sergio.florez@fundacionctic.org>
2025-04-22 14:57:38 +0200

    fix: vite configuration to deploy dev in remote server

--- front/lec-app/vite.config.js
+++ front/lec-app/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: ['localhost', 'newdevit'],
+    allowedHosts: ['localhost', 'newdevit'],
     port: 5173,
     watch: {
       usePolling: true
